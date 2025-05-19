f5dd2d8256a00de456549ae64b675b001212a97c
pasha <pashatat47@gmail.com>
2025-05-19 23:23:30 +0300

    tasks done

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { deepStrictEqual } from 'node:assert';
+import { readContacts } from './readContacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = await readContacts();
+  const contacts = JSON.parse(data);
+
+  //smart check: if added array(from generate contacts) then add it, if just 1 (from add-one) then just push it
+  const newContacts = Array.isArray(updatedContacts)
+    ? [...contacts, ...updatedContacts] // merge arrays
+    : [...contacts, updatedContacts]; // push single item
+
+  await fs.writeFile(
+    PATH_DB,
+    JSON.stringify(newContacts, undefined, 2),
+    'utf-8',
+  );
+};
